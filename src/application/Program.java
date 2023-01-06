0a3b2b2398e05e106bc22f065e18711f518a21e6
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-01-05 23:03:44 -0300

    Adicao da classe Seller e conclusao do teste na Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "DP_Teste"); 
-		System.out.println("Teste: " + obj.toString());
+		Department dpto = new Department(1, "DP_Teste"); 
+		Seller sell = new Seller(1, "SL_Teste", "", new Date(), 0.0, dpto); 
+		
+		System.out.println("Teste dpto: " + dpto.toString());
+		System.out.println("Teste sell: " + sell.toString());
 	}
 }
