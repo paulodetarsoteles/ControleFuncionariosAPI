e5903d93843ff365907ab5c67f140e7f1900716a
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-01-14 19:47:22 -0300

    Teste na classe program

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		
 		Department dpto = new Department(1, "DP_Teste"); 
 		Seller sell = new Seller(1, "SL_Teste", "", new Date(), 0.0, dpto); 
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
 		System.out.println("Teste dpto: " + dpto.toString());
 		System.out.println("Teste sell: " + sell.toString());
