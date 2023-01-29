acda562374f2199791530f6cb19c48f9c297704a
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-01-29 20:44:12 -0300

    Implantação da busca de vendedor por departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,7 +15,13 @@ public class Program {
 		
 		System.out.println("*** TEST1: Find Seller By Id: ");
 		Seller seller = sellerDao.findById(3); 
-		
 		System.out.println(seller);
+		
+		System.out.println("\n*** TEST2: Find Seller By Department: ");
+		Department department = new Department(2, null); 
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
